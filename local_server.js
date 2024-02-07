dbdd454623346f9fdaadae1c9c53cdf1b726590a
Elias Crum <49316983+ecrum19@users.noreply.github.com>
2024-02-07 15:56:25 +0100

    Fixed the exec command for relaunchability of the local pod

--- local_server.js
+++ local_server.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 
 function Solid_server_init() {
-    exec('npx @solid/community-server -p 3000 -c @css:config/file.json', (error, stdout, stderr) => {
+    exec('npx @solid/community-server -p 3000 -c @css:config/file.json -f .data', (error, stdout, stderr) => {
         if (error) {
         return console.error(`Error: ${error.message}`);
         } 
