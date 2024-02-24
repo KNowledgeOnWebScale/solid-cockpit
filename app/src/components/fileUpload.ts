52cc9c82793abc864fd21774f53c3f96f0fb4bd6
ecrum19 <elicrum88@gmail.com>
2024-02-24 20:19:17 +0100

    small changes

--- app/src/components/fileUpload.ts
+++ app/src/components/fileUpload.ts
@@ -1,8 +1,13 @@
 import { saveFileInContainer, WithResourceInfo } from '@inrupt/solid-client';
-import { fetch } from "@inrupt/solid-client-authn-browser";
+import { Session, fetch } from "@inrupt/solid-client-authn-browser";
 
-export async function uploadFile(file: File): Promise<void> {
+async function uploadFile(file: File): Promise<void> {
     // Replace 'https://your-pod.solidcommunity.net/public/' with the URL of your container
-    const savedFile = await saveFileInContainer('http://localhost:3000', file, { fetch: window.fetch }) as File & WithResourceInfo;
+    const savedFile = await saveFileInContainer('http://localhost:3000/data', file, { fetch: window.fetch }) as File & WithResourceInfo;
     console.log(`Saved ${savedFile}`);
+}
+
+async function checkFilesInPod() {
+    // Use the Solid JavaScript Client to fetch the files
+    
 }
