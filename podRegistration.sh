5b9824d93c998acd49249e68a489ba16c06fc4d6
Elias Crum <eliascrum@OS-Bessje.local>
2024-05-16 13:53:41 +0200

    pod registration script changes

--- podRegistration.sh
+++ podRegistration.sh
@@ -3,16 +3,12 @@
 # get current dir and navigate to the user's WebID card
 read -p "Enter the name of your pod: " pod
 current_directory=$(pwd)
-for _ in 1; do
-    echo "$current_directory"
-    current_directory=$(dirname "$current_directory")
-done
 
 # designates the file to add the Pod info to
 target_directory="$current_directory/.data/$pod/profile"
 file_name="card$.ttl"
 full_path="$target_directory/$file_name"
-
+echo $full_path
 # check to make sure the directory is accurate
 if [ ! -d "$target_directory" ]; then
     echo "Error: $target_directory does not exist..."
@@ -22,7 +18,7 @@ fi
 # replaces the "." with a ";" for ttl format continuity
 to_replace="a foaf:Person."
 fixed="a foaf:Person;"
-sed -i "s@$to_replace@$fixednew-text@g" "$full_path"
+sed -i '' -e "s|$to_replace|$fixed|g" "$full_path"
 
 # makes it visible where your pod is using <../pim/space#storage>
-#echo "    <http://www.w3.org/ns/pim/space#storage> <../> ." >> $full_path
+echo "    <http://www.w3.org/ns/pim/space#storage> <../>." >> $full_path
