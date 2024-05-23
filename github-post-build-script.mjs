a896c5e9481a3c62f2ef90c3a4f135dd6b1faf4e
Elias Crum <eliascrum@OS-Bessje.local>
2024-05-23 15:50:27 +0200

    route problems begone

--- github-post-build-script.mjs
+++ github-post-build-script.mjs
@@ -11,5 +11,5 @@ const routes = [
 
 
 for (const route of routes) {
-  cpSync("/dist" + "/index.html", "/dist" + route + "/index.html");
+  cpSync("/TRIPLE_App/dist" + "/index.html", "/TRIPLE_App/dist" + route + "/index.html");
 }
