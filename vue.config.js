c936a5d5de54b37d5347f7e4a0c1fb8a272f8735
ecrum19 <elicrum88@gmail.com>
2024-05-24 13:14:58 +0200

    no more routing issues, please?

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? 'TRIPLE_App' : '/',
-  outputDir: '/dist'
+  outputDir: '../dist'
 });
 
 
