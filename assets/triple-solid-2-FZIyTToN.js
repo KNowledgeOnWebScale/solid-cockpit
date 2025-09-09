be5ff48a3d79166d7d17583cf8870276da30ef6c
ecrum19 <ecrum19@users.noreply.github.com>
2025-09-09 13:39:03 +0000

    Deploying to pages from @ KNowledgeOnWebScale/solid-cockpit@ebf4834b0a1f4da44f1670a0139fe09c03eca5cf 🚀

--- assets/triple-solid-2-FZIyTToN.js
+++
@@ -1,8 +0,0 @@
-const t=`# Datasources: http://localhost:3000/test/Uploads/nbn-chist-era-annex-1-chemicals-alt.ttl
-PREFIX thd: <urn:triple-hybrid-demo:>
-SELECT DISTINCT ?CAS WHERE {
-
-  # Solid pod data
-  ?SOLIDPOD thd:predicate ?CAS .
-
-}`;export{t as default};
