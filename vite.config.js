cb33742361ffb6d9aff5899c033304eb921140b3
ecrum19 <elicrum88@gmail.com>
2025-09-16 09:56:56 +0200

    added 404.html file after build step to handle gh-pages routing issues (2)

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base: process.env.VITE_BASE_URL || './',
-  transpileDependencies: true,
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
+export default defineConfig(({ command }) => {
+  const isDev = command === "serve";
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
-})
+    base: isDev ? "./" : "/solid-cockpit/",
+    transpileDependencies: true,
+    build: {
+      outDir: "dist",
+    },
+    server: {
+      headers: {
+        "Cross-Origin-Opener-Policy": "same-origin",
+        "Cross-Origin-Embedder-Policy": "require-corp",
+      },
+    },
+  };
+});
